9095537f3ad6f02998264d96c63c12634cf57c67
Kevin Chen <kyc5cw@virginia.edu>
2013-10-28 15:49:59 -0400

    operator.py

---
+++ operator.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import socket
+import sys
+from thread import thread
+	
+#super class
+class Operator:
+	def __init__(self,host_name,port):
+		self.port = port
+		self.host_name = host_name
+	
+	
+class Messenger(Operator):
+	
+	def _connect(self):
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.connect(self.host_name,self.port)
+	
+	#returns true if message is sent completely
+	def send(self,message):
+		try:
+			self._connect()
+			status = self.sock.sendall(message)
+			self.sock.close()
+		except socket.error:
+			#in case of failure, intiate a resend protocol
+			return False
+		
+		return True
+		
+class Reciever(Operator):
+	
+	def setup(self):
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.bind(host,port)
+	
+	def client_handle(self, connection):
+		#message list
+		message_raw = []
+		while True:
+			data = connection.recv(4096)
+			if not data:
+				break
+			#append message to list
+			message_raw.append(data)
+			
+		#concatenate to single string
+		message = ''.join(message_raw)
+		
+		#add check for delimiter here (TBD)
+		
+		#parse in dispatch function
+		self.dispatch(message)
+	
+	#begins listening
+	def spin(self):
+		#up to 8 concurrent connections
+		self.sock.listen(8)
+		while 1:
+			#fire off a thread to accept and handle incoming messages
+			conn, addr = self.sock.accept()
+			start_new_thread(client_handle,((conn,))
+			
+		s.close()
+		
