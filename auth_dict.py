2d6e3a91dddda56932f2b54264d3e97422fb446b
hq5na <hq5na@virginia.edu>
2013-10-30 16:17:23 -0400

    from alex

--- auth_dict.py
+++ auth_dict.py
@@ -19,8 +19,10 @@ def createUser(login,passwd):
     tmp = False
     with open('userinfo.pickle', 'rb') as handle:
         reader = pickle.load(handle)
-        if reader.has_key(login):   return False
-        else:   tmp = True
+        if reader.has_key(login):
+            return False
+        else:
+            tmp = True
     if tmp == True:
         with open('userinfo.pickle','wb') as handle:
             reader[login] = passwd
@@ -41,15 +43,19 @@ while True:
         # extract username and password from line
         username = line[:line.find(' ')]
         password = line[line.find(' ')+1:]
-        if matchpasswd(username, password):     print 'Successfully Logged in\n'
-        else:    print 'Login Failed\n'
+        if matchpasswd(username, password):
+            print 'Successfully Logged in\n'
+        else:
+            print 'Login Failed\n'
     elif command == 'create':
         print "Type in 'Username Password'"
         line = sys.stdin.readline()
         line = line.strip()
         usernameCreate = line[:line.find(' ')]
         passwordCreate = line[line.find(' ')+1:]
-        if createUser(usernameCreate, passwordCreate):  print 'User Created\n'
-        else:   print 'ERR. User exists\n'
+        if createUser(usernameCreate, passwordCreate):
+            print 'User Created\n'
+        else:
+            print 'ERR. User exists\n'
     elif command == 'exit':
         break
