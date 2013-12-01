d6e4613f25e4eb430cdbdfe363d3e7959a390672
David <drl4eu@virginia.edu>
2013-11-30 23:33:27 -0500

    Some Server-Side Share Logic

--- Server/ServerFileUpdateManager.py
+++ Server/ServerFileUpdateManager.py
@@ -29,10 +29,11 @@ class ServerFileUpdateManager():
         self.global_info.global_cur_user_id = str(self.global_info.active_user_directory[token].user_id)
         if(event == "~UPDATE~"):
             self.send_update_list(item[2], token)
-        elif event == "~SYNCON~":
-            print "Need to implement turning sync on!"
-        elif event == "~SYNCOFF~":
-            print "Need to implement turning sync off!"
+        elif event == "~SHARE~":
+            #Need to add some way of verifying that the share token is correct
+            #Issue arises when user sharing isn't logged in
+            #self.send_update_list("2010-11-18 12:29:15.998000", sharetoken)
+            print "Share Logic"
         else:
             with open(self.global_info.server_global_directory + self.global_info.global_cur_user_id + "\\EventLog.txt", "a") as f:
                 f.write("\n" + event[:event.find(">") + 1])
